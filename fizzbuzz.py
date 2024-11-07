e8cd1d60496e1b251776e28823843d9ec9d390ec
Euclides <Euclides@ALIEN>
2024-11-07 14:24:06 -0500

    Result of fizzbuzz exercise

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,23 @@
 # add your code here
+for x in range(1, 10):
+    # select number to print 
+    number = x
+    # check if is fizz
+    number_is_fizz = number % 3 == 0
+    # check if is buzz
+    number_is_buzz = number % 5 == 0
 
+    # Build string to print
+    str_to_print = ""
+
+    if number_is_fizz:
+        str_to_print += "Fizz"
+    
+    if number_is_buzz:
+        str_to_print += "Buzz"
+    
+    # if string is empty print number
+    if str_to_print == "":
+        str_to_print = str(number)
+
+    print(str_to_print)
